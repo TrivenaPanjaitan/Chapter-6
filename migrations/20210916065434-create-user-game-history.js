985e4a1f2d91063ba5732b6df894f05485e51478
@trivenaynpanjaitan <trivenapanjaitan7.1.3@gmail.com>
2021-09-20 23:30:58 +0700

    update tri

---
+++ migrations/20210916065434-create-user-game-history.js
@@ -0,0 +1,39 @@
+'use strict'
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('user_game_histories', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      point: {
+        type: Sequelize.INTEGER,
+      },
+      total_play: {
+        type: Sequelize.INTEGER,
+      },
+      id_user_game: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: {
+            tableName: 'user_game',
+          },
+          key: 'id',
+        },
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    })
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('user_game_histories')
+  },
+}
